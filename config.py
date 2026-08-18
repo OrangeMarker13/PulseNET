fd9c9caa5989910429f71f509f8c9df8dde90cfb
OrangeMarker13 <pranittayal@gmail.com>
2026-08-17 23:35:12 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,92 @@
+SUPPORTED_CITIES = {
+    "Charlotte, NC": {
+        "county": "Mecklenburg County",
+        "state": "North Carolina"
+    },
+    "Raleigh, NC": {
+        "county": "Wake County",
+        "state": "North Carolina"
+    },
+    "Greensboro, NC": {
+        "county": "Guilford County",
+        "state": "North Carolina"
+    },
+    "Durham, NC": {
+        "county": "Durham County",
+        "state": "North Carolina"
+    },
+    "Winston-Salem, NC": {
+        "county": "Forsyth County",
+        "state": "North Carolina"
+    },
+    "Fayetteville, NC": {
+        "county": "Cumberland County",
+        "state": "North Carolina"
+    },
+    "Cary, NC": {
+        "county": "Wake County"
+    },
+    "Wilmington, NC": {
+        "county": "New Hanover County"
+    },
+    "High Point, NC": {
+        "county": "Guilford County"
+    },
+    "Concord, NC": {
+        "county": "Cabarrus County"
+    },
+    "Asheville, NC": {
+        "county": "Buncombe County"
+    },
+    "Gastonia, NC": {
+        "county": "Gaston County"
+    },
+    "Chapel Hill, NC": {
+        "county": "Orange County"
+    },
+    "Jacksonville, NC": {
+        "county": "Onslow County"
+    },
+    "Rocky Mount, NC": {
+        "county": "Nash County"
+    },
+    "Burlington, NC": {
+        "county": "Alamance County"
+    },
+    "Wilson, NC": {
+        "county": "Wilson County"
+    },
+    "Greenville, NC": {
+        "county": "Pitt County"
+    },
+    "Kannapolis, NC": {
+        "county": "Cabarrus County"
+    },
+    "Apex, NC": {
+        "county": "Wake County"
+    },
+    "Huntersville, NC": {
+        "county": "Mecklenburg County"
+    },
+    "Mooresville, NC": {
+        "county": "Iredell County"
+    }
+}
+
+DEFAULT_AMBULANCE_COUNT = 12
+
+MAX_AMBULANCE_COUNT = 100
+
+DEFAULT_ANALYSIS_MODE = "Current conditions"
+
+ANALYSIS_MODES = [
+    "Current conditions",
+    "Historical conditions",
+    "Custom scenario"
+]
+
+DATA_FALLBACK_ORDER = [
+    "live",
+    "recent",
+    "historical"
+]
