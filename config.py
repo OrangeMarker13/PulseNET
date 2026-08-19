070fa827852347737eeb49a864b61f84029a4b92
OrangeMarker13 <pranittayal@gmail.com>
2026-08-18 22:06:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,82 +1,30 @@
 SUPPORTED_CITIES = {
-    "Charlotte, NC": {
-        "county": "Mecklenburg County",
-        "state": "North Carolina"
-    },
-    "Raleigh, NC": {
-        "county": "Wake County",
-        "state": "North Carolina"
-    },
-    "Greensboro, NC": {
-        "county": "Guilford County",
-        "state": "North Carolina"
-    },
-    "Durham, NC": {
-        "county": "Durham County",
-        "state": "North Carolina"
-    },
-    "Winston-Salem, NC": {
-        "county": "Forsyth County",
-        "state": "North Carolina"
-    },
-    "Fayetteville, NC": {
-        "county": "Cumberland County",
-        "state": "North Carolina"
-    },
-    "Cary, NC": {
-        "county": "Wake County"
-    },
-    "Wilmington, NC": {
-        "county": "New Hanover County"
-    },
-    "High Point, NC": {
-        "county": "Guilford County"
-    },
-    "Concord, NC": {
-        "county": "Cabarrus County"
-    },
-    "Asheville, NC": {
-        "county": "Buncombe County"
-    },
-    "Gastonia, NC": {
-        "county": "Gaston County"
-    },
-    "Chapel Hill, NC": {
-        "county": "Orange County"
-    },
-    "Jacksonville, NC": {
-        "county": "Onslow County"
-    },
-    "Rocky Mount, NC": {
-        "county": "Nash County"
-    },
-    "Burlington, NC": {
-        "county": "Alamance County"
-    },
-    "Wilson, NC": {
-        "county": "Wilson County"
-    },
-    "Greenville, NC": {
-        "county": "Pitt County"
-    },
-    "Kannapolis, NC": {
-        "county": "Cabarrus County"
-    },
-    "Apex, NC": {
-        "county": "Wake County"
-    },
-    "Huntersville, NC": {
-        "county": "Mecklenburg County"
-    },
-    "Mooresville, NC": {
-        "county": "Iredell County"
-    }
+    "Charlotte, NC": {"county": "Mecklenburg County", "state": "North Carolina"},
+    "Raleigh, NC": {"county": "Wake County", "state": "North Carolina"},
+    "Greensboro, NC": {"county": "Guilford County", "state": "North Carolina"},
+    "Durham, NC": {"county": "Durham County", "state": "North Carolina"},
+    "Winston-Salem, NC": {"county": "Forsyth County", "state": "North Carolina"},
+    "Fayetteville, NC": {"county": "Cumberland County", "state": "North Carolina"},
+    "Cary, NC": {"county": "Wake County", "state": "North Carolina"},
+    "Wilmington, NC": {"county": "New Hanover County", "state": "North Carolina"},
+    "High Point, NC": {"county": "Guilford County", "state": "North Carolina"},
+    "Concord, NC": {"county": "Cabarrus County", "state": "North Carolina"},
+    "Asheville, NC": {"county": "Buncombe County", "state": "North Carolina"},
+    "Gastonia, NC": {"county": "Gaston County", "state": "North Carolina"},
+    "Chapel Hill, NC": {"county": "Orange County", "state": "North Carolina"},
+    "Jacksonville, NC": {"county": "Onslow County", "state": "North Carolina"},
+    "Rocky Mount, NC": {"county": "Nash County", "state": "North Carolina"},
+    "Burlington, NC": {"county": "Alamance County", "state": "North Carolina"},
+    "Wilson, NC": {"county": "Wilson County", "state": "North Carolina"},
+    "Greenville, NC": {"county": "Pitt County", "state": "North Carolina"},
+    "Kannapolis, NC": {"county": "Cabarrus County", "state": "North Carolina"},
+    "Apex, NC": {"county": "Wake County", "state": "North Carolina"},
+    "Huntersville, NC": {"county": "Mecklenburg County", "state": "North Carolina"},
+    "Mooresville, NC": {"county": "Iredell County", "state": "North Carolina"}
 }
 
 DEFAULT_AMBULANCE_COUNT = 12
-
 MAX_AMBULANCE_COUNT = 100
-
 DEFAULT_ANALYSIS_MODE = "Current conditions"
 
 ANALYSIS_MODES = [
@@ -90,3 +38,19 @@ DATA_FALLBACK_ORDER = [
     "recent",
     "historical"
 ]
+
+OSM_SEARCH_RADIUS_KM = 20
+ROUTING_TIMEOUT = 15
+NOMINATIM_TIMEOUT = 10
+WEATHER_TIMEOUT = 10
+OVERPASS_TIMEOUT = 30
+
+MAX_QUANTUM_QUBITS = 12
+QUANTUM_SHOTS = 512
+QUANTUM_REPETITIONS = 1
+
+RESPONSE_THRESHOLD_MINUTES = 10
+
+DEMAND_GRID_SIZE = 5
+DEMO_RECORD_COUNT = 500
+RANDOM_SEED = 42
